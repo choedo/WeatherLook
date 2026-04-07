90eda63f403053597cfc3e8e47d3c3493fdc140f
choedo <96doyeong@naver.com>
2026-04-07 20:30:43 +0900

    이미지 생성 커맨드 변경

--- src/services/ai/fetchRecommendFashion.ts
+++ src/services/ai/fetchRecommendFashion.ts
@@ -7,7 +7,7 @@ export async function fetchRecommendFashion(weatherInfo: string) {
 
   const response = await ai.models.generateImages({
     model: 'imagen-4.0-generate-001',
-    prompt: `${weatherInfo} 날씨에 어울리는 옷을 입은 커플 룩북 스타일`,
+    prompt: `${weatherInfo} 날씨에 어울리는 옷을 입은 커플 룩북 스타일을 모두 16:9 비율로 제작해주고 배경은 꽉차게, 인물은 가운데 있도록 해줘.`,
     config: {
       numberOfImages: 4,
       aspectRatio: '16:9',
