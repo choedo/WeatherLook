4717315dd15218ef3f80997f1d8b89b6df4c1721
choedo <96doyeong@naver.com>
2026-04-09 13:48:29 +0900

    인덱스 페이지 반응형 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,24 +41,43 @@ export default function Home() {
     }
   }, [coords, setLocation]);
 
+  const isLoadingLocation = !coords;
+
   return (
     <div className={'h-screen flex flex-col'}>
-      <div className={'flex-1 grid grid-cols-12 gap-4 max-w-7xl mx-auto'}>
+      <div
+        className={`
+          flex-1 
+          grid grid-cols-12 gap-4
+          max-w-7xl mx-auto
+        `}
+      >
         <div
-          className={
-            'col-span-7 p-8 flex flex-col items-start justify-center gap-12'
-          }
+          className={`
+            col-span-12 lg:col-span-7
+            px-4 py-8 lg:px-8
+            flex flex-col items-start justify-center gap-4 lg:gap-12 md:gap-8
+          `}
         >
           <WeatherKbd text={'실시간 날씨 정보'} />
 
           <div className={'flex flex-col gap-4'}>
-            <h1 className={'text-6xl font-extrabold'}>
+            <h1
+              className={`
+                text-2xl lg:text-6xl md:text-4xl
+                font-extrabold
+              `}
+            >
               Your Day,{' '}
               <i className={'text-(--azure-sky-700)'}>Perfectly Styled</i> by
               the Sky.
             </h1>
             <p
-              className={'text-lg text-muted-foreground font-medium break-keep'}
+              className={`
+                text-xs lg:text-lg md:text-sm
+                text-muted-foreground 
+                font-medium 
+                break-keep`}
             >
               Weather Look은 복잡한 기상 데이터를 분석해 당신의 라이프스타일에
               딱 맞는 옷차림을 추천합니다. 날씨 때문에 고민하는 일 없는 완벽한
@@ -67,41 +86,42 @@ export default function Home() {
           </div>
 
           <ul className={'flex gap-4 items-start'}>
-            <li
-              className={
-                'flex flex-col gap-1 px-8 py-4 rounded-full bg-[#E5F2FF]'
-              }
-            >
-              <ShirtIcon fill={'#402D00'} />
-              <h6 className={'text-lg font-bold'}>스마트 스타일링</h6>
-              <p className={'text-xs text-muted-foreground'}>
-                기온과 날씨에 맞춘 최적의 코디 추천
-              </p>
-            </li>
-
-            <li
-              className={
-                'flex flex-col gap-1 px-8 py-4 rounded-full bg-(--azure-sky-600)/30'
-              }
-            >
-              <ArchiveIcon className={'text-(--azure-sky-700)'} />
-              <h6 className={'text-lg font-bold'}>데일리 필수템</h6>
-              <p className={'text-xs text-muted-foreground'}>
-                날씨에 따라 꼭 챙겨야 할 아이템 가이드
-              </p>
-            </li>
+            <ListItem
+              icon={<ShirtIcon fill={'#402D00'} />}
+              title={'스마트 스타일링'}
+              content={'기온과 날씨에 맞춘 최적의 코디 추천'}
+            />
+            <ListItem
+              icon={<ArchiveIcon className={'text-(--azure-sky-700)'} />}
+              title={'데일리 필수템'}
+              content={'날씨에 따라 꼭 챙겨야 할 아이템 가이드'}
+            />
           </ul>
 
           <Button
             onClick={handleNavigation}
+            onMouseEnter={() => router.prefetch(`/${path}`)}
             size={'lg'}
             variant={'customDefault'}
-            className={'rounded-full px-12 py-8 text-xl font-bold'}
+            disabled={isLoadingLocation}
+            className={`
+              rounded-full 
+              px-8 py-6 lg:px-12 md:px-10 md:py-8
+              text-sm lg:text-xl md:text-lg
+              font-bold`}
           >
             날씨 조회
           </Button>
         </div>
-        <div className={'col-span-5 p-8 w-full h-2/3 relative my-auto'}>
+        <div
+          className={`
+            lg:col-span-5
+            p-8 
+            w-full aspect-4/5 my-auto
+            relative 
+            hidden lg:block
+          `}
+        >
           <Image
             src={'/index-image.png'}
             alt={'Weather Look 소개 이미지'}
@@ -113,5 +133,37 @@ export default function Home() {
 
       <Footer />
     </div>
+  );
+}
+
+function ListItem({
+  icon,
+  title,
+  content,
+}: {
+  icon: React.ReactElement<{ className?: string }>;
+  title: string;
+  content: string;
+}) {
+  return (
+    <li
+      className={`
+        flex flex-col gap-1 
+        px-6 py-4 lg:px-8
+        rounded-full
+        bg-[#E5F2FF]
+      `}
+    >
+      <div className={`flex gap-1 md:flex-col`}>
+        {React.cloneElement(icon, {
+          className: `w-3 h-3 lg:w-6 lg:h-6 md:w-5 md:h-5`,
+        })}
+
+        <h6 className={`text-xs lg:text-lg md:text-sm font-bold break-keep`}>
+          {title}
+        </h6>
+      </div>
+      <p className={`text-xs text-muted-foreground`}>{content}</p>
+    </li>
   );
 }
--- src/components/WeatherKbd.tsx
+++ src/components/WeatherKbd.tsx
@@ -8,13 +8,27 @@ interface Props {
 export default function WeatherKbd({ text }: Props) {
   return (
     <div
-      className={`flex items-center gap-2 
-              bg-(--golden-amber-500) 
-              py-2 px-3 
-              rounded-full`}
+      className={`
+        flex items-center gap-2 
+        bg-(--golden-amber-500) 
+        py-2 px-3 
+        rounded-full  
+      `}
     >
-      <SunDimIcon fill={'#402D00'} size={16} />
-      <p className={'text-sm text-[#402D00] font-bold'}>{text}</p>
+      <SunDimIcon
+        fill={'#402D00'}
+        size={16}
+        className={'w-3 h-3 md:w-4 md:h-4'}
+      />
+      <p
+        className={`
+        text-xs md:text-sm
+        text-[#402D00] 
+        font-bold
+        `}
+      >
+        {text}
+      </p>
     </div>
   );
 }
